5507470f24cfd123d9483e028a92ce9e60998f4d
William Wright <williamwright5191@gmail.com>
2021-06-11 13:45:23 -0400

    added to frame test

--- Initial testing/frame_test.py
+++ Initial testing/frame_test.py
@@ -2,3 +2,17 @@ import tkinter as tk
 
 window = tk.Tk()
 
+frame1 = tk.Frame()
+frame2 = tk.Frame()
+
+label1 = tk.Label(master=frame1, text="This is Frame 1")
+label1.pack()
+
+label2 = tk.Label(master=frame2, text="This is Frame 2")
+label2.pack()
+
+frame1.pack()
+frame2.pack()
+
+
+window.mainloop()
