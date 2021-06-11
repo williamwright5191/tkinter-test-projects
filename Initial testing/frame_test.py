1acd1a5b2a1de24ff7e7ecd8e9376f0ea3d029b9
William Wright <williamwright5191@gmail.com>
2021-06-11 14:00:09 -0400

    playing with pack() command

--- Initial testing/frame_test.py
+++ Initial testing/frame_test.py
@@ -1,18 +1,19 @@
 import tkinter as tk
+from tkinter.constants import TRUE
 
 window = tk.Tk()
 
 frame1 = tk.Frame()
 frame2 = tk.Frame()
 
-label1 = tk.Label(master=frame1, text="This is Frame 1")
-label1.pack()
+label1 = tk.Label(master=frame1, text="This is Frame 1", fg="black", bg="orange")
+label1.pack(fill=tk.BOTH, expand=TRUE)
 
-label2 = tk.Label(master=frame2, text="This is Frame 2")
-label2.pack()
+label2 = tk.Label(master=frame2, text="This is Frame 2", fg="green", bg="white")
+label2.pack(fill=tk.BOTH, expand=TRUE)
 
-frame1.pack()
-frame2.pack()
+frame1.pack(fill=tk.BOTH, expand=TRUE)
+frame2.pack(fill=tk.BOTH, expand=TRUE)
 
 
 window.mainloop()
